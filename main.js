2e830e6b91df316d066df7fb81a0eb7b0af119fc
qknapper <knapper.quinn@gmail.com>
2017-11-02 10:04:42 -0400

    sdf

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-$(window).ready(updateHeight);
-$(window).resize(updateHeight);
+$(
 
-updateHeight() 
+function() 
 {
     var div = $('A3');
     var width = div.width();
     
     div.css('height', (1.40964*width));
 }
+
+);
