6c346ad85d56170465e535a781872a5c98c0b6cc
qknapper <knapper.quinn@gmail.com>
2018-08-27 18:26:40 -0400

    resume png update and funzone tic tac toe update

--- ticTacToe.js
+++ ticTacToe.js
@@ -15,40 +15,116 @@ function fillSpace(num)
 	var s33 = document.getElementById('33');
 	var sentinel = true;
 		
+	//	User turn
 	current.innerHTML = 1;
-	square.innerHTML = '<img src="QbearSquare.png" alt="QuinnBearSquare" title="QuinnBearSquare" style="width:90%; height:90%" border="2">';
-	if(checkWin())
+	if(!checkWin())
 	{
-		response.innerHTML = "You win! Congratulations!!! &ensp; Refresh to play again!";
-		sentinel = false;
-	}
-	
-	// AI - Random
-	ai = String(Math.floor(Math.random() * 3 + 1)) + String(Math.floor(Math.random() * 3 + 1));
-	var aiCurrent;
-	var aiSquare;
-	var empty;
-	var catsGame = parseInt(s11.innerHTML) + parseInt(s12.innerHTML) + parseInt(s13.innerHTML)
-					+ parseInt(s21.innerHTML) + parseInt(s22.innerHTML) + parseInt(s23.innerHTML)
-					+ parseInt(s31.innerHTML) + parseInt(s32.innerHTML) + parseInt(s33.innerHTML);
-	
-	while(sentinel && (catsGame < 13))
-	{
-		aiCurrent = document.getElementById(ai);
-		aiSquare = document.getElementById('s' + ai);
-		empty = (aiCurrent.innerHTML == 0);
-		if(empty)
+		square.innerHTML = '<img src="QbearSquare.png" alt="QuinnBearSquare" title="QuinnBearSquare" style="width:90%; height:90%" border="2">';
+		if(checkWin())
 		{
-			aiCurrent.innerHTML = 2;
-			aiSquare.innerHTML = '<img src="trogdorSquare.png" alt="trogdorSquare" title="trogdorSquare" style="width:90%; height:90%" border="2">';
-			if(checkWin()) 
-				response.innerHTML = '<br> HA, sucker! You lose :( Refresh to try again!';
-			sentinel = false;
+			response.innerHTML = "<br> You win! Congratulations!!! &ensp; Refresh to play again!";
 		}
-		else ai = String(Math.floor(Math.random() * 3 + 1)) + String(Math.floor(Math.random() * 3 + 1));
+		else
+			setTimeout(
+				function()
+				{
+					//	Cats Game
+					var catsGame = parseInt(s11.innerHTML) + parseInt(s12.innerHTML) + parseInt(s13.innerHTML)
+								+ parseInt(s21.innerHTML) + parseInt(s22.innerHTML) + parseInt(s23.innerHTML)
+								+ parseInt(s31.innerHTML) + parseInt(s32.innerHTML) + parseInt(s33.innerHTML);
+					if(catsGame < 13)
+					{
+					
+						/*	AI - Algorithm
+									Less optimal moves come first and are overwritten if a more optimal move is found
+			
+											Col1	Col2	Col3
+									Row1	 11		12		13
+									Row2	 21		22		23
+									Row3	 31		32		33
+										Diag2					Diag1	*/
+						
+						var ai;
+						var aiCurrent, aiSquare;
+						var board = 
+						[
+							[parseInt(s11.innerHTML), parseInt(s12.innerHTML), parseInt(s13.innerHTML)],
+							[parseInt(s21.innerHTML), parseInt(s22.innerHTML), parseInt(s23.innerHTML)],
+							[parseInt(s31.innerHTML), parseInt(s32.innerHTML), parseInt(s33.innerHTML)]
+						];	
+				
+			
+						//	i) Random Space
+						while(sentinel)
+						{
+							ai = String(Math.floor(Math.random() * 3 + 1)) + String(Math.floor(Math.random() * 3 + 1));
+							aiCurrent = document.getElementById(ai);
+							if(aiCurrent.innerHTML == 0)
+								sentinel = false;
+						}
+					
+						//	ii) Play in middle
+						if(board[1][1] == 0)
+							ai = "22";
+						
+						//	iii) Checks for a winning move for either team.
+					
+						
+						var team;
+						var rowCount, colCount;
+						var d1Count = 0;
+						var d2Count = 0;
+						var i, j;
+				
+						for(team = 1; team < 3; team++)
+						{
+							
+							d1Count = 0;
+							d2Count = 0;
+							
+							for(i = 0; i < board.length; i++)
+							{
+								colCount = 0;
+								rowCount = 0;
+						
+								for(j = 0; j < board[i].length; j++)
+								{
+									if(board[i][j] == team)
+										rowCount++;
+									if(board[j][i] == team)
+										colCount++;
+									if(i == j && board[i][j] == team)
+										d1Count++;
+									if(i == (2-j) && board[i][j] == team)
+										d2Count++;
+								}
+								
+								for(j = 0; j < board.length; j++)
+								{
+									if(rowCount > 1 && board[i][j] == 0)
+										ai = String(i+1) + String(j+1);
+									if(colCount > 1 && board[j][i] == 0)	
+										ai = String(j+1) + String(i+1);
+									if(i == j && d1Count > 1 && board[i][j] == 0)
+										ai = String(i+1) + String(j+1);
+									if(i == (2-j) && d2Count > 1 && board[i][j] == 0)
+										ai = String(i+1) + String(j+1);
+								}
+							}
+						}
+					
+						aiCurrent = document.getElementById(ai);
+						aiSquare = document.getElementById('s' + ai);
+					
+						aiCurrent.innerHTML = 2;
+						aiSquare.innerHTML = '<img src="trogdorSquare.png" alt="trogdorSquare" title="trogdorSquare" style="width:90%; height:90%" border="2">';
+						if(checkWin())
+							response.innerHTML = "<br> You lose :( &ensp; Refresh for a rematch!!";
+					}
+					else
+						response.innerHTML = "<br> It's a tie... Refresh to try again!";
+				}, (Math.random()*1000));
 	}
-	
-	if((catsGame >= 13) && !(checkWin())) response.innerHTML = "<br>It's a kitty kat's game XD... Refresh to try again!";
 }
 
 function checkWin()
