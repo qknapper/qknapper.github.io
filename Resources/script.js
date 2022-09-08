96c6cff014df664b1b4d0e0ecc8a7a99bbbdeb0a
Quinn Knapper <knapper.quinn@gmail.com>
2022-09-07 21:56:58 -0400

    a1 woorld fixes

--- Resources/script.js
+++ Resources/script.js
@@ -26,7 +26,6 @@ function zoomZone()
 	var version = document.getElementById("version").value;
 	var level = document.getElementById("level").value;
 	var message = document.getElementById("message").value;
-	var pword = document.getElementById("pword").value;
 
-	window.open('mailto: qbear@qbear.org?subject=A1 Mario World Zoom Zone&body=I am an awesome gamer and I beat the A1 Mario World Special World - put my name on your website!%0D%0A %0D%0A- Name: [ ' + name + ' ]%0D%0A- Game Version: [ ' + version + ' ]%0D%0A- Favorite Level: [ ' + level + ' ]%0D%0A- Message: [ ' + message + ' ]%0D%0A- Password: [ ' + pword + ' ]%0D%0A%0D%0AThank you so much qbear! You and your Super Mario World romhack are awesome!%0D%0ALots of love, [ ' + name + ' ]');
+	window.open('mailto: qbear@qbear.org?subject=A1 Mario World Zoom Zone&body=I am an awesome gamer and I beat the A1 Mario World Special World - put my name on your website!%0D%0A %0D%0A- Name: [ ' + name + ' ]%0D%0A- Game Version: [ ' + version + ' ]%0D%0A- Favorite Level: [ ' + level + ' ]%0D%0A- Message: [ ' + message + ' ]%0D%0A%0D%0AThank you so much qbear! You and your Super Mario World romhack are awesome!%0D%0ALots of love, [ ' + name + ' ]');
 }
